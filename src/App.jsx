adc4c9a209c98cd309b6ad8dc6160a3a9d0e3e17
João  Lima <joao.lima@somoseducacao.com.br>
2022-12-12 11:05:21 -0300

    created array data of posts

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,37 @@ import { Sidebar } from './components/Sidebar'
 import styles from './App.module.css'
 import './global.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/jvrapi.png',
+      name: 'João Vitor',
+      role: 'Web Developer'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: '👉 jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2022-12-12 09:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/jvrapi.png',
+      name: 'João Vitor',
+      role: 'Analista de Desenvolvimento de sistemas'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: '👉 jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2022-12-11 09:00:00')
+  },
+]
+
 function App() {
 
   return (
@@ -14,10 +45,17 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-            author='João Vitor'
-            content='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, ex harum quasi officiis, temporibus, molestiae debitis assumenda porro dicta doloremque exercitationem quo quisquam accusamus fuga iusto amet error obcaecati non!'
-          />
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+                
+              />
+            )
+          })}
         </main>
       </div>
     </div>
