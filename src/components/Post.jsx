7462681985181268882b2b4cfab8901f37b9dbff
João  Lima <joao.lima@somoseducacao.com.br>
2022-12-12 15:42:47 -0300

    feature: add validation in textarea of new comment

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -31,7 +31,12 @@ export function Post({author, publishedAt, content}){
   }
 
   function handleNewCommentChange(){
+    event.target.setCustomValidity('')
     setNewCommentText(event.target.value)
+  }
+
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity('Esse campo é obrigatório!')
   }
 
   function deleteComment(commentToDelete){
@@ -40,6 +45,8 @@ export function Post({author, publishedAt, content}){
     })
     setComments(commentsWithoutDeleteOne)
   }
+
+  const isNewCommentEmpty = newCommentText.length === 0
 
   return (
      <article className={styles.post}>
@@ -76,11 +83,15 @@ export function Post({author, publishedAt, content}){
             name='comment'
             value={newCommentText}
             onChange={handleNewCommentChange} 
+            onInvalid={handleNewCommentInvalid}
             placeholder='Deixe um comentário'
+            required
           />
 
           <footer>
-            <button type='submit'>Publicar</button>
+            <button type='submit' disabled={isNewCommentEmpty}>
+              Publicar
+            </button>
           </footer>
 
         </form>
